fabc1acdd0e9ce191296982aad02ccd63be8b7a1
Santiago Toso <santiagotoso@gmail.com>
2026-09-05 09:10:57 -0300

    correccion token, cambio de puerto a 4000 para no conflictar con front, actualizacion env.example y cors en main

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ import { Reflector } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    credentials: true, // necesario si en algún momento usás cookies (ej. httpOnly para el JWT)
+  });
+
+
   const config = new DocumentBuilder()
     .setTitle('Campus Lite API')
     .setDescription('Documentación de la API de Campus Lite')
@@ -19,6 +26,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); // disponible en /api
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,7 +15,10 @@ import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UserRole } from './entities/user.entity';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
